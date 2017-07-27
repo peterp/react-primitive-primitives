401424c2e8a61caa53393390dc91df1de40d5fe9
Peter Pistorius <peter.pistorius@gmail.com>
2017-07-27 16:56:33 +0200

    v0.1.0

---
+++ packages/react-primitives-web/src/helpers.js
@@ -0,0 +1,10 @@
+export const flattenStyle = (style, defaultStyle = {}) => {
+  const flatStyle = Array.isArray(style)
+    ? style.reduce((o, s) => ({ ...o, ...s }), {})
+    : style;
+
+  return {
+    ...flatStyle,
+    ...defaultStyle
+  };
+};
