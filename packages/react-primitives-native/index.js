85883080511d31a00683e04b61044e0c50c1d461
Peter Pistorius <peter.pistorius@gmail.com>
2017-07-27 15:17:01 +0200

    Remove bad jest configuration

--- packages/react-primitives-native/index.js
+++ packages/react-primitives-native/index.js
@@ -1,7 +1,9 @@
-export {
-  TouchableOpacity as Touchable,
-  View,
-  Text,
-  Image,
-  Platform
-} from "react-native";
+var RN = require("react-native");
+
+module.exports = {
+  Touchable: RN.TouchableOpacity,
+  View: RN.View,
+  Text: RN.Text,
+  Image: RN.Image,
+  Platform: RN.Platform
+};
