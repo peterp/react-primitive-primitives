85fe1a914d64f795f59b45c23f98f8c692f6e0d8
Peter Pistorius <peter.pistorius@gmail.com>
2018-03-26 16:42:12 +0200

    Make GradientView get size from layout.

--- packages/react-primitives-native/src/GradientView.js
+++ packages/react-primitives-native/src/GradientView.js
@@ -1,34 +1,49 @@
 import React from "react";
-import { ART, Dimensions } from "react-native";
 import PropTypes from "prop-types";
+import { View, ART } from "react-native";
 
-const GradientView = ({ colors, locations, start, end, ...others }) => {
-  const { height, width } = Dimensions.get("window");
+class GradientView extends React.Component {
+  state = {
+    width: 0,
+    height: 0
+  };
 
-  const fillMap = locations.reduce((newColors, location, i) => {
-    return {
-      ...newColors,
-      [location]: colors[i]
-    };
-  }, {});
+  render() {
+    const { colors, locations, start, end, style, ...others } = this.props;
+    const { width, height } = this.state;
 
-  return (
-    <ART.Surface width={width} height={height} {...others}>
-      <ART.Shape
-        d={`M0,0 L${width},0 L${width},${height} L0,${height} Z`}
-        fill={
-          new ART.LinearGradient(
-            fillMap,
-            start.x * width,
-            start.y * height,
-            end.x * width,
-            end.y * height
-          )
-        }
-      />
-    </ART.Surface>
-  );
-};
+    const fillMap = locations.reduce((newColors, location, i) => {
+      return {
+        ...newColors,
+        [location]: colors[i]
+      };
+    }, {});
+    return (
+      <View
+        style={[style, { overflow: "hidden" }]}
+        onLayout={({ nativeEvent: { layout: { width, height } } }) => {
+          this.setState({ width, height });
+        }}
+        {...others}
+      >
+        <ART.Surface width={width} height={height} fill="red">
+          <ART.Shape
+            d={`M0,0 L${width},0 L${width},${height} L0,${height} Z`}
+            fill={
+              new ART.LinearGradient(
+                fillMap,
+                start.x * width,
+                start.y * height,
+                end.x * width,
+                end.y * height
+              )
+            }
+          />
+        </ART.Surface>
+      </View>
+    );
+  }
+}
 
 GradientView.propTypes = {
   start: PropTypes.object.isRequired,
